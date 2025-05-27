334eb9811ab42418f92657b002e21a22ce6b4ce1
Amir <mramirrhm@gmail.com>
2025-05-27 16:53:42 +0330

    leo

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/multiform-login-page/', // ← Add this with YOUR repo name
   build: {
-    outDir: 'dist', // Explicit output directory
-  },
-  base:'/multiform-login-page', // Base public path
+    outDir: 'dist',
+  }
 })
